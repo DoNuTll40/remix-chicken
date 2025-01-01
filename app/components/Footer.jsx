aa7bb587dcfdcb9f94948c7b8b16e7453ab562a0
DoNuT_ll40 <nuttawoot.ch64@snru.ac.th>
2025-01-01 19:30:07 +0700

    upload code

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     <div className="mt-20 font-noto-thai">
         <hr />
         <div className="mt-5 mb-10 mx-auto max-w-[80rem] flex justify-between">
-            <p>&copy; AFK Chicken 2022,Inc</p>
+            <p>&copy; AFK Chicken 2022-2025,Inc</p>
             <div className="flex gap-3">
                 <a href="/">หน้าหลัก</a>
                 <a href="#option">ฟังก์ชั่น</a>
